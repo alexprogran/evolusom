824afe052f2b770ef864f38d198e3ee33e9921b6
Alex <alexprogran@gmail.com>
2025-10-12 06:22:45 -0300

    Iplementações no SobreSection

--- src/components/SobreSection.jsx
+++ src/components/SobreSection.jsx
@@ -6,28 +6,30 @@ const SobreSection = () => {
     <section id="sobre" className="sobre">
       <div className="container">
         <div className="sobre-header">
-          <h2 className="section-title">Sobre a Evolu<span className="brand-s">S</span>om</h2>
+        <img 
+        src="/logo.png" 
+        alt="EvoluSom Logo" 
+        className="sobre-logo" 
+      />
+          {/* <h2 className="section-title">Sobre a Evolu<span className="brand-s">S</span>om</h2> */}
           <div className="title-underline"></div>
         </div>
 
         <div className="sobre-content">
           <div className="sobre-texto">
             <p className="sobre-paragrafo">
-              Com mais de 10 anos de experiência no mercado, a <strong><span className="brand">evolusom</span></strong> é referência em qualidade,
-              tecnologia e atendimento personalizado. Nosso compromisso é elevar o padrão de conforto
-              e segurança em cada projeto automotivo, residencial e empresarial.
+            Há mais de 10 anos oferecemos soluções completas e inovadoras, desde a instalação de películas de proteção
+            e sistemas de som até serviços elétricos, instalação de multimídias e acessórios de alta qualidade. Temos uma
+            equipe especializada, pronta para lhe atender.
+                
             </p>
-            <p className="sobre-paragrafo">
-              Oferecemos soluções completas e inovadoras, desde a instalação de películas de proteção
-              e sistemas de som até serviços elétricos, instalação de multimídias e acessórios de alta qualidade. Nossa equipe
-              especializada trabalha com dedicação para transformar seu espaço e veículo.
-            </p>
+            
 
             <div className="sobre-diferenciais">
               <div className="diferencial-card">
                 <Shield className="diferencial-icon" size={40} />
-                <h3>Garantia de Qualidade</h3>
-                <p>Produtos e serviços certificados com garantia estendida</p>
+                <h3>Garantia</h3>
+                <p>Produtos e serviços certificados com garantia </p>
               </div>
               <div className="diferencial-card">
                 <Award className="diferencial-icon" size={40} />
@@ -45,8 +47,7 @@ const SobreSection = () => {
           <div className="sobre-video">
             <div className="video-wrapper">
             <video className="sobre-video-player" controls preload="metadata" playsInline>
-              <source src="/apresent_1.mp4" type="video/mp4" />
-              <source src="/quem_somos.MOV" type="video/quicktime" />
+              <source src="/apresent_1.mp4" type="video/mp4" />             
               Seu navegador não suporta a tag de vídeo.
             </video>
             </div>
