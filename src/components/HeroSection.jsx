515a1530b434932ed41f6dacccf2f096c9f80e99
Alex <alexprogran@gmail.com>
2025-10-25 06:07:30 -0300

    Alterações no HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,17 +11,13 @@ const HeroSection = () => {
 
   return (
     <section id="inicio" className="hero">
-      <video autoPlay loop muted playsInline className="hero-video">
-        <source src="/hero_vd.mp4" type="video/mp4" />
-      </video>
       <div className="hero-overlay"></div>
       <img  
-        src="/logo.png" 
-     
+        src="/logo.png"      
         alt="EvoluSom Logo" 
         className="hero-logo" 
       />
-      <div className="hero-content"> 
+      {/* <div className="hero-content"> 
         <h1 className="hero-title">
         Películas, multimídias e acessórios 
         </h1>
@@ -49,7 +45,7 @@ const HeroSection = () => {
           </button>
         </div>
         
-      </div>
+      </div> */}
       
       <div className="hero-scroll-indicator">
         <ChevronDown size={32} className="scroll-icon" />
